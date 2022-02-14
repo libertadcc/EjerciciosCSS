5cce2c82025e0ba68cb195735b6ed9529b0fdf28
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2022-02-14 19:45:16 +0100

    html css

---
+++ EJERCICIOSJS/area/main.js
@@ -0,0 +1,37 @@
+// let lado1 = 16;
+// var lado2 = 23;
+// const lado3 = 13;
+
+// var volumen = lado1*lado2*lado3;
+
+// console.log('El volumen es de', volumen);
+
+
+// console.log('H'+ 'O'+'L'+'A');
+
+// console.log(`1Los valores de los lados son ${lado1}, ${lado2} y ${lado3}`);
+// console.log('2Los valores de los lados son ' + lado1+ ',' +lado2 +' y ' + lado3 );
+
+// const cumple = {
+//     day: 5,
+//     month: 'april'
+// };
+
+// console.log('Mi cumple es ' + cumple.day + ' de ' + cumple['month']);
+
+let numero2 = 8
+let numero1 = 5;
+
+
+if ( numero2 < 0 || numero2 >=1 ) {
+    console.log(numero2);
+}
+
+// El incrementar en una unidad el valor de numero1 no lo hace mayor o igual que numero2
+if( numero1++ <= numero2) {
+    console.log('si')
+}
+
+let name = prompt('dime tu nombre');
+
+console.log('name', name);
---
+++ EJERCICIOSJS/ex1/main.js
@@ -0,0 +1,9 @@
+alert('Soy un alert!');
+
+let office = "Calle"
+
+console.log('Esto en consola');
+
+console.log('Mensaje', office);
+
+console.log(office);
